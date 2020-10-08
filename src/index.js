2e9c41b5cbd06304588cf2884eac2c9c240e252e
JustinNSmith <Smithjn@g.cofc.edu>
2020-10-07 20:48:56 -0400

    :sparkles: Add Finshed Checkbox

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const footerTemplate = html`
 `;
 
 class TodoApp extends LitElement {
+  static get properties() {
+    return { todos: { type: Array } };
+  }
   constructor() {
     super();
     this.todos = [
@@ -19,16 +22,42 @@ class TodoApp extends LitElement {
   render() {
     return html`
       <h1>Todo App</h1>
+
+      <input id="addTodoInput" placeholder="Name" />
+      <button @click="${this._addTodo}">Add</button>
       <ol>
         ${this.todos.map(
           (todo) =>
             html`<li>
-              ${todo.text} (${todo.finished ? "finished" : "unfinished"})
+              <input
+                type="checkbox"
+                .checked=${todo.finished}
+                @change="${(e) => this._changeTodoFinished(e, todo)}"
+              />
+              ${todo.text}
+              <button @click=${() => this._removeTodo(todo)}>X</button>
             </li>`
         )}
       </ol>
       ${footerTemplate}
     `;
+  }
+  _addTodo() {
+    const input = this.shadowRoot.getElementById("addTodoInput");
+    const text = input.value;
+    input.value = "";
+
+    this.todos = [...this.todos, { text, finished: false }];
+  }
+  _removeTodo(removeTodo) {
+    this.todos = this.todos.filter((todo) => todo !== removeTodo);
+  }
+  _changeTodoFinished(e, changedTodo) {
+    const finished = e.target.checked;
+
+    this.todos = this.todos.map((todo) =>
+      todo !== changedTodo ? todo : { ...changedTodo, finished }
+    );
   }
 }
 
